2bb5e3803721a43d6854a922fd6af318c5cb5e14
jarleborn <h.jarleborn@gmail.com>
2019-07-03 13:13:20 +0200

    build gh

--- build/main.js
+++ build/main.js
@@ -93,9 +93,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   // res.sendFile(__dirname + '/public/index.html')
-  res.sendfile(__dirname + '/public/index.html');
+  //res.sendfile(__dirname + '/public/index.html')
+  res.send('hej');
 });
 
+app.get('/ping', function (req, res) {
+  res.send('bajskorv');
+});
 app.post('/getinfo', function (req, res) {
   console.log(req.body);
   if (req.body.series) {
